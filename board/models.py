19fe355d44fd36afb9cd5828d55b2c479d483b15
vibsin9322 <vibsin74@gmail.com>
2025-02-28 15:04:26 +0900

    board생성

--- board/models.py
+++ board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=20)
+    writer = models.CharField(max_length=10)
+    content = models.TextField()
+    file = models.FileField()
+    created_at = models.DateTimeField(auto_now=True)
+    read_count = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return f"Board[id={self.id}, title={self.title}]"
+
+    def add_count(self):
+        self.read_count +=1
+        self.save()
+        
