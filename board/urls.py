2b9dab1dc07f50bc0dea9b9e998df98e5d312c1f
vibsin9322 <vibsin74@gmail.com>
2025-02-28 16:52:50 +0900

    수정삭제 완료

--- board/urls.py
+++ board/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
         path("",views.List.as_view(), name="list"),
         path("detail/<int:pk>/", views.Detail.as_view(), name="detail"),
         path("create/", views.Create.as_view(), name="create"),
+        path("edit/<int:pk>/", views.Edit.as_view(), name ="edit"),
+        path("delete/<int:pk>/", views.Delete.as_view(), name="delete")
 ]
--- board/views.py
+++ board/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 from .models import Board
+from django.urls import reverse, reverse_lazy
 
 # class Create(CreateView):
 #     model = Board
@@ -13,10 +14,16 @@ class List(ListView):
 class Detail(DetailView):
     model = Board
     
+    def get_object(self):
+        object = super().get_object()
+        object.add_count()
+        return object
+    
+    
 class Create(CreateView):
     model = Board
     fields = ["title","writer","content","file"]
-    success_url = "/board/"
+    success_url = reverse_lazy("board:list")
     
     def get_form(self):
         form = super().get_form()
@@ -25,3 +32,34 @@ class Create(CreateView):
         form.fields["content"].label = "내용"
         form.fields["file"].label = "파일"
         return form
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["button_lbl"] ="쓰기"
+        return context
+    
+    
+    
+class Edit(UpdateView):
+    model = Board
+    fields = ["title","writer","content","file"]
+    success_url = reverse_lazy("board:list")
+    
+    def get_form(self):
+        form = super().get_form()
+        form.fields["title"].label = "제목"
+        form.fields["writer"].label = "작성자"
+        form.fields["writer"].disabled = True
+        form.fields["content"].label = "내용"
+        form.fields["file"].label = "파일"
+        return form
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["button_lbl"] ="수정"
+        return context
+    
+class Delete(DeleteView):
+    model = Board
+    success_url = reverse_lazy("board:list")
+    
